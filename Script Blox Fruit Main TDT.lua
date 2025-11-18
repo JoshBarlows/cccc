ebe8bc6c641ad0da9a317b3900f99fed9ecaccea
JoshBarlows <ada.magalhaes@hotmail.com>
2025-11-18 12:44:42 +0700

    Update Script Blox Fruit Main TDT.lua

--- Script Blox Fruit Main TDT.lua
+++ Script Blox Fruit Main TDT.lua
@@ -44,7 +44,7 @@ Pirates = function()
     replicated.Remotes.CommF_:InvokeServer("SetTeam", "Pirates")
 end
 
-local TeddyGay = loadstring(game:HttpGet("https://pastefy.app/THGq6A8p/raw"))()
+local TeddyGay = loadstring(game:HttpGet("https://pastefy.app/oRwYCYhi/raw"))()
 
 local DoorBySea = {
 
